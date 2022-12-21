26db8643b1b2582dd2e24056aee0a8668c887706
joelrodrigue2016 <joelrodrigue2016@fau.edu>
2022-12-21 13:01:58 -0500

    up and running

---
+++ javaGUI/src/javaGUI/file.java
@@ -0,0 +1,8 @@
+package javaGUI;
+
+public class file {
+	public static void main(String[] args) {
+		System.out.println("it works");
+	}
+
+}
---
+++ javaGUI/src/javaGUI/fileTest.java
@@ -0,0 +1,94 @@
+package javaGUI;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class fileTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	final void testMain() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testNotify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testNotifyAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testWait() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testWaitLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testWaitLongInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testFinalize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
