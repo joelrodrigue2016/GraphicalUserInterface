865681bddd4bd6d2e5696fc42c92a9a0b5486a47
joelrodrigue2016 <joelrodrigue2016@fau.edu>
2022-12-21 21:33:40 -0500

    Sqlite up and working

--- javaGUI/src/sqlite/SQLITEConnection.java
+++ javaGUI/src/sqlite/SQLITEConnection.java
@@ -2,6 +2,7 @@ package sqlite;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 /**
  * 
@@ -11,16 +12,46 @@ import java.sql.DriverManager;
 public class SQLITEConnection {
 	public static Connection connect() {
 		Connection conn = null;
-		String dbname = "personalInfo";
+		String dbname = "personal";
 		try {
 
 			Class.forName("org.sqlite.JDBC");
 			conn = DriverManager.getConnection("jdbc:sqlite:" + dbname + ".sqlite");
+			conn.setAutoCommit(false);
+			Statement stmt = conn.createStatement();
 			System.out.println("Connected successfully!!");
+			//// DROP TABLE IF EXISTS
+			String sql = "CREATE TABLE if not exists personalInfo " + "(  ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+					+ " NAME           TEXT    NOT NULL, " + " LASTNAME           TEXT    NOT NULL,"
+					+ " NATIONALITY           TEXT, " + " AGE            INT     NOT NULL," + " ADDRESS         TEXT, "
+					+ " SCHOOL       TEXT)";
+			stmt.executeUpdate(sql);
+			System.out.println("table created successfully!!");
+			stmt = conn.createStatement();
+			String sqlinput = "INSERT INTO personalInfo (NAME,LASTNAME,NATIONALITY,AGE,ADDRESS,SCHOOL) "
+					+ "VALUES ('Paul','Perez', 'American','32', 'California','University of Florida' );";
+
+			stmt.executeUpdate(sqlinput);
+			String sqlinput1 = "INSERT INTO personalInfo (NAME,LASTNAME,NATIONALITY,AGE,ADDRESS,SCHOOL) "
+					+ "VALUES ('Paul','Perez', 'American','32', 'California','University of Florida' );";
+
+			stmt.executeUpdate(sqlinput1);
+			String sqlinput2 = "INSERT INTO personalInfo (NAME,LASTNAME,NATIONALITY,AGE,ADDRESS,SCHOOL) "
+					+ "VALUES ('Paul','Perez', 'American','32', 'California','University of Florida' );";
+			stmt.executeUpdate(sqlinput2);
+
+			String sqlinput3 = "INSERT INTO personalInfo (NAME,LASTNAME,NATIONALITY,AGE,ADDRESS,SCHOOL) "
+					+ "VALUES ('Joel','Rodriguez', 'American','35', 'Florida','Florida Atlantic University' );";
+			stmt.executeUpdate(sqlinput3);
+
+			stmt.close();
+			conn.commit();
+			conn.close();
+			System.out.println("Data entered successfully!!");
 
 		} catch (Exception e) {
 			// TODO: handle exception
-			System.out.println(e);
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 		}
 		return conn;
 
--- javaGUI/src/sqlite/SqliteMain.java
+++ javaGUI/src/sqlite/SqliteMain.java
@@ -9,6 +9,7 @@ public class SqliteMain {
 /////////daabase is located inside the project folder
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		SQLITEConnection.connect();
 	}
 
