9383eee3238e2e8b2dcb9a0438587f39dcfcf464
joelrodrigue2016 <joelrodrigue2016@fau.edu>
2022-12-24 19:25:06 -0500

    ok

---
+++ javaGUI/src/formulas/Main.java
@@ -0,0 +1,15 @@
+package formulas;
+
+public class Main {
+
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Formulas formulas = new Formulas();
+
+		System.out.println(formulas.velocity(52, 89));
+		System.out.println(formulas.distance(2, 6));
+		System.out.println(formulas.time(5, 9));
+	}
+
+}
